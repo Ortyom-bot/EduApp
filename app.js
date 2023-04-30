2e93fb6dc03085c0690b281f72dea2c4bd6f5567
artem <artembitsa6@gmail.com>
2023-04-30 22:11:32 +0300

    test3

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const path = require('path')
+const fs = require('fs')
 const app = express()
 const fileUpload = require('express-fileupload')
 
@@ -8,9 +10,11 @@ const hbs = exphbs.create({
     defaultLayout: 'main',
     extname: 'hbs'
 })
+
 app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
+
 // Статичная папка
 app.use(express.static('./public'))
 
@@ -22,16 +26,12 @@ app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
 // Загрузка файлов на сервер
 app.use(fileUpload())
-
-// Роуты
-app.use('/register', require('./routes/register'))
-app.use('/auth', require('./routes/auth'))
-app.use('/upload', require('./routes/upload'))
-app.use('/comments', require('./routes/comments'))
-app.use('/api', require('./routes/api'))
-app.use('/', require('./routes/pages'))
 
 app.listen(3000, () => {
     console.log('Сервер запущен')
